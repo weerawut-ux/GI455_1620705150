fbb647bc4b7154dc8ed3fffcffabf35cb3b42bbd
LAPTOP-N449OIKN\weerawut <weerawut.boon@bumail.net>
2021-01-28 13:39:20 +0700

    Send Assignment Week1

---
+++ GI455_Unity Project/Assets/Code/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string[] ListData = {"Phone" , "Pepsi" , "Milk" , "Glade" , "Yakult" , "Pen"};
+    public string inputName;
+    public InputField inputField ;
+    public Text textDisplay;
+    
+
+    public void CheckName(){
+        
+        inputName = inputField.text;
+
+        if (ListData.Contains(inputName))
+        {
+            textDisplay.text = "<color=#337DFF>" + inputName + "</color> Is Found";
+        }
+        else
+        {
+            textDisplay.text = "<color=#CD5C5C>" + inputName + "</color> Is Not Found";
+        }
+
+    }
+}
+
+    
+
+    
+
+    
